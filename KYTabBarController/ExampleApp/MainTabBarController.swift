899c6b0c6d3bce4461b4bfce422ad54cc8f0f8af
Kyrie SIU <siulong1996@gmail.com>
2020-08-13 20:33:41 +0800

    Removed xcuserstate

--- KYTabBarController/ExampleApp/MainTabBarController.swift
+++ KYTabBarController/ExampleApp/MainTabBarController.swift
@@ -23,19 +23,19 @@ class MainTabBarController: KYTabBarController {
         // Do any additional setup after loading the view.
         let routeVC = SampleViewController()
         //        let evemtstabBarItem = KYTabBarItem.init(KYTabBarItemImageContentView(image: #imageLiteral(resourceName: "menu"), selectedImage: #imageLiteral(resourceName: "menu")), title: "Events", tag: 0)
-        let routeTabBarItem = KYTabBarItem.init(KYTabBarItemLottieContentView(animationName: "tab_route_light", placeholderImage:#imageLiteral(resourceName: "menu")), title: "Route", tag: 0)
+        let routeTabBarItem = KYTabBarItem.init(KYTabBarItemLottieContentView(animationName: "tab_route_dark", placeholderImage:#imageLiteral(resourceName: "menu")), title: "Route", tag: 0)
         routeTabBarItem.selectedTintColor = #colorLiteral(red: 0.9843137255, green: 0.7254901961, blue: 0.1803921569, alpha: 1)
         routeVC.tabBarItem = routeTabBarItem
         
         let ycVC = SampleViewController()
         //        let evemtstabBarItem = KYTabBarItem.init(KYTabBarItemImageContentView(image: #imageLiteral(resourceName: "menu"), selectedImage: #imageLiteral(resourceName: "menu")), title: "Events", tag: 0)
-        let ycTabBarItem = KYTabBarItem.init(KYTabBarItemLottieContentView(animationName: "tab_yc_light", placeholderImage:#imageLiteral(resourceName: "menu")), title: "YC", tag: 0)
+        let ycTabBarItem = KYTabBarItem.init(KYTabBarItemLottieContentView(animationName: "tab_yc_dark", placeholderImage:#imageLiteral(resourceName: "menu")), title: "YC", tag: 0)
         ycTabBarItem.selectedTintColor = #colorLiteral(red: 0.9843137255, green: 0.7254901961, blue: 0.1803921569, alpha: 1)
         ycVC.tabBarItem = ycTabBarItem
         
         let settingsVC = SampleViewController()
         //        let evemtstabBarItem = KYTabBarItem.init(KYTabBarItemImageContentView(image: #imageLiteral(resourceName: "menu"), selectedImage: #imageLiteral(resourceName: "menu")), title: "Events", tag: 0)
-        let settingsTabBarItem = KYTabBarItem.init(KYTabBarItemLottieContentView(animationName: "tab_settings_light", placeholderImage:#imageLiteral(resourceName: "menu")), title: "Settings", tag: 0)
+        let settingsTabBarItem = KYTabBarItem.init(KYTabBarItemLottieContentView(animationName: "tab_settings_dark", placeholderImage:#imageLiteral(resourceName: "menu")), title: "Settings", tag: 0)
         settingsTabBarItem.selectedTintColor = #colorLiteral(red: 0.9843137255, green: 0.7254901961, blue: 0.1803921569, alpha: 1)
         settingsVC.tabBarItem = settingsTabBarItem
         
